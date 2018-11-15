0dedf6ae7e927f19645dae2bf5ec1b52c2f1ad3e
Julia Gu <Juejing.Gu@yh.nackademin.se>
2018-11-15 13:20:09 +0100

    Julia ny

--- src/GameServer/Dao.java
+++ src/GameServer/Dao.java
@@ -12,25 +12,25 @@ import quizkampen.QandA;
  * @author Julia
  */
 public class Dao {
-    QandA histryQ1 = new QandA("history", "what is the year?", 
+    Questions histryQ1 = new Questions("history", "what is the year?", 
             new String[]{"1999", "1990", "1992", "1998"}, "1998", "");
     
-    QandA histryQ2 = new QandA("history", "what is the person?", 
+    Questions histryQ2 = new Questions("history", "what is the person?", 
             new String[]{"Linkon", "Fox", "Ranbon", "Franklin"}, "Fox", "");
     
-    QandA naturalQ1 = new QandA("natural", "what is the weather?", 
+    Questions naturalQ1 = new Questions("natural", "what is the weather?", 
             new String[]{"winter", "summer", "autom", "spring"}, "winter", "");
     
-    QandA naturalQ2 = new QandA("natural", "what is the water?", 
+    Questions naturalQ2 = new Questions("natural", "what is the water?", 
             new String[]{"hot", "cold", "iced", "steam"}, "iced", "");
-    List<QandA> qAndAn = new ArrayList<>();
+    List<Questions> qAndAn = new ArrayList<>();
     public Dao(){
         qAndAn.add(histryQ1);
         qAndAn.add(histryQ2);
         qAndAn.add(naturalQ1);
         qAndAn.add(naturalQ2);
     }
-    public QandA getByNum(int num){
+    public Questions getByNum(int num){
         for(int i=0; i<4; i++ ){
             if(i+1 == num){
                 return qAndAn.get(i);
