3e56e69105963a0396bec4bdbd4710926ac38efc
Josef <Josef.NymanMuller@yh.nackademin.se>
2018-11-15 10:50:05 +0100

    Merge origin/Develop into Develop
    Conflicts:
    	src/inlämningsuppgift3/inlämningsuppgift3/GameLogic.java

--- src/QuizClient/QuizMain.java
+++ src/QuizClient/QuizMain.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class QuizMain extends Application {
