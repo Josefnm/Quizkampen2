787460e632b470212f16150469a4a0c028670069
Johnny Hoang <johnny.hoang@yh.nackademin.se>
2018-11-15 13:59:40 +0100

    QuestionScene basics done

--- src/GameClient/QuestionScene.java
+++ src/GameClient/QuestionScene.java
@@ -3,28 +3,37 @@ package GameClient;
 import java.util.ArrayList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javax.swing.JButton;
 
 
 public class QuestionScene {
 
     Scene scene;
+    BorderPane border = new BorderPane();
     GridPane grid = new GridPane(); 
     
     QuestionScene(){   
-        ArrayList<JButton> buttons = new ArrayList<>();
     
         for(int i = 0; i<2; i++){            
             for(int j = 0; j<2; j++){
-                Button button = new Button("");
+                Button button = new Button("Svar:");
+                button.setMinSize(50,50);
                 button.setOnAction(click);
                 grid.add(button, i, j);
             }
         }
-        Scene scene = new Scene(grid,300,300);
+        Text text = new Text("Frågan som ställs");
+        border.setTop(text);
+        grid.setAlignment(Pos.CENTER);
+        border.setCenter(grid);
+        
+        this.scene = new Scene(border,300,300);
     }
     
     
@@ -35,4 +44,7 @@ public class QuestionScene {
         }
     };
     
+    public Scene getScene(){
+        return scene;
+    }
 }
