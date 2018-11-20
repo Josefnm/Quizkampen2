62ef7d3b8468720819dcff232aabeb50868fe854
relaxmeister <jojjethebest@hotmail.com>
2018-11-20 14:56:08 +0100

    Merge JonasFiva into Develop
    Conflicts:
    	src/GameClient/QuestionScene.java

--- src/GameClient/StartScene.java
+++ src/GameClient/StartScene.java
@@ -1,7 +1,12 @@
 package GameClient;
 
+import GameServer.Question;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -41,9 +46,9 @@ public class StartScene { //fixar abstrakt senare
         //label 1 "användarnamn"
         //label 2 "actual användarnamn"-betydligt större font
         //Avatarbild, den ska även gå att ändra
-        
+
         Image avatarBoy = new Image("./images/boy.png"); //en getter från server i slutändan?
-        
+
         ImageView avatar = new ImageView();
         avatar.setImage(avatarBoy);
         avatar.setFitWidth(40);
@@ -59,15 +64,21 @@ public class StartScene { //fixar abstrakt senare
         //lambda
         //tanken att vi genom start engagear servern
         startbtn.setOnAction(e -> {
-        client.sendObject("start");
-        //main.setMainScene();
-        //main.setScoreScene();
-        popUp(main); 
-        System.out.println("hejdå");
+            client.sendObject("start");
+            try {
+                //main.setMainScene();
+                //main.setScoreScene();
+                popUp(main);
+            } catch (IOException ex) {
+                Logger.getLogger(StartScene.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(StartScene.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            //main.setScoreScene();
+            System.out.println("hejdå");
         }); //hej funkar 100%
         //bonus, onclick, popop med spinning wheel + cancelknapp vars funktion stänger popupen
-        
-        
+
         //borderpane, vbox, hbox
         BorderPane BP = new BorderPane();
         BP.setId("pane");
@@ -75,9 +86,7 @@ public class StartScene { //fixar abstrakt senare
         HBox hbox = new HBox(avatar, vbox);
         HBox hboxKnapp = new HBox(startbtn);
         VBox vboxAllt = new VBox(hbox, hboxKnapp);
-        
-        
-        
+
         hbox.setAlignment(Pos.CENTER);
         vbox.setAlignment(Pos.CENTER);
         hboxKnapp.setAlignment(Pos.CENTER);
@@ -85,19 +94,16 @@ public class StartScene { //fixar abstrakt senare
 
         //BP.setBottom(hboxKnapp);
         BP.setCenter(vboxAllt);
-        
+
         //BP.setTop(startbtn);
         //BP.setBottom(avatar);
-        
 //        show.setOnAction(e -> {
 //        System.out.println("hejdu");
 //        main.closePopupStage();
 //        System.out.println("nukörvi");
 //        });
-        
-
         this.startScene = new Scene(BP, main.getBoardHeight(), main.getBoardThicc());
-        
+
         startScene.getStylesheets()
                 .add(StartScene.class
                         .getResource("stylingCSS.css").toExternalForm());
@@ -106,46 +112,62 @@ public class StartScene { //fixar abstrakt senare
     public Scene getScene() {
         return startScene;
     }
-    public void popUp(Main main) throws IOException, ClassNotFoundException
-    {
+
+    public void popUp(Main main) throws IOException, ClassNotFoundException {
         BorderPane bp = new BorderPane();
         bp.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;");
         //bp.setId("score-test");
-        popupScene = new Scene(bp, (main.getBoardHeight()/2), (main.getBoardThicc()/2));
-        
+        popupScene = new Scene(bp, (main.getBoardHeight() / 2), (main.getBoardThicc() / 2));
+
         //adda progressbar 
         //popup.show(primaryStage);
-        
         Button show = new Button("Cancel");
         Text searching = new Text("Letar efter spelare...");
         ProgressIndicator pi = new ProgressIndicator();
         VBox pop = new VBox(searching, pi, show);
         pop.setStyle("-fx-margin: 20");
-        
+
         pop.setAlignment(Pos.CENTER);
         pop.setSpacing(5);
         bp.setCenter(pop);
-        
+
         System.out.println("HEHEHEHEH");
         main.setPopupScene(popupScene);
-        
-        
+
         //lambdaknappaction
         show.setOnAction(e -> {
             System.out.println("hejdu");
-        main.closePopupStage();
-        System.out.println("nukörvi");
+            main.closePopupStage();
+            System.out.println("nukörvi");
         });
-        
-        Object input=client.getInStream().readObject();
-        main.setQuestionScene();
-        
-        
+        System.out.println("before thread");
+        new Thread(() -> { //annars hängde sig popupen
+            System.out.println("thread start");
+            try {
+                ArrayList<Question> input = (ArrayList) client.getInStream().readObject();
+                System.out.println("input");
+            } catch (IOException ex) {
+                Logger.getLogger(StartScene.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(StartScene.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            Platform.runLater(() -> {
+                main.setQuestionScene();
+                //TODO
+                //
+                //qs.setQuestion(input);
+                main.closePopupStage();
+            });
+        }).start();
+//        Object input=client.getInStream().readObject();
+//        main.setQuestionScene();
+
         //skapa popupruta
         //innehåller progressbar
         //samt knapp cancel
         //kopplad till boolean
     }
+
     public Scene getPopupScene() {
         System.out.println("kom tillbaka");
         return popupScene;
@@ -181,8 +203,6 @@ public class StartScene { //fixar abstrakt senare
 //        }
 //    }
 //}
-
-
 //        startbtn.setOnAction(new EventHandler<ActionEvent>() {
 //
 //            
